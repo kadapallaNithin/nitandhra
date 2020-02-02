7c9b7c70eedf08b336ac536a0c045bb1c8c0adfe
kadapallaNithin <42470278+kadapallaNithin@users.noreply.github.com>
2020-02-02 13:51:42 +0530

    Create parser.h

---
+++ LanguageProcessors/parser/parser.h
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+#define MAX_SI 300
+class Parser{
+	public:
+		char input[300];
+		int i;
+		int len;
+
+		Parser(){
+			i = 0;
+			len = 0;
+		}
+		bool match(char ch){
+			if(ch == input[i]){
+				i++;
+				return true;
+			}else{
+				cout<<"Error "<<ch<<endl;				
+				return false;
+			}
+		}
+		void read(char deli){
+			char c = ' ';
+			while(c != deli && len < MAX_SI){
+				c = getchar();
+				input[i++] = c;
+				len++;
+			}
+		}
+		void read(){
+			read('\n');
+		}
+		void parse(){
+			i = 0;
+		}
+		/*void read_parse(){
+			read();
+			parse();
+//			pre();
+		}*/
+};
