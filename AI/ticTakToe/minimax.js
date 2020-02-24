9c6cf76dba191db357cb6529ab5254e477a53fe4
nithin <kadapallanithin@gmail.com>
2020-02-24 23:32:35 +0530

    added functions for mini and max

--- AI/ticTakToe/minimax.js
+++ AI/ticTakToe/minimax.js
@@ -1,5 +1,35 @@
 //JavaScript
-function minimax(root){
-	console.log('done');
+function mini(root){
+	if(root.children.length > 0){
+		minkey = root.children[0].key;
+	  minim = root.children[0].obj[0];
+	  for(var child in root.children){
+		if(minim > child.obj[0]){
+		  minim = child.obj[0];
+		  minkey = child.key;
+		}
+	  }
+	  return [minim,minkey];
+	}
+	return [];
 }
+function max(root){
+	if(root.children.length > 0){
+		maxkey = root.children[0].key;
+	  	maxim = root.children[0].obj[0];
+	  	for(var child in root.children){
+			if(maxim < child.obj[0]){
+		  		maxim = child.obj[0];
+		  		maxkey = child.key;
+			}
+	  	}
+	  	return [maxim,maxkey];
+	}
+	return []
+}
+
+function minimax(root){
+
+}
+
 
